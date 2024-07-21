053df50f1271954a8129ec219d628a987c2ec20e
choi tae hwoon <chlxogns@tukorea.ac.kr>
2024-07-21 16:44:55 +0900

    Add files via upload

---
+++ led.py
@@ -0,0 +1,50 @@
+try:
+    import RPi.GPIO as GPIO     # Import Raspberry Pi GPIO module
+except ImportError:
+    from mock_gpio import GPIO  # Import mock GPIO module
+
+import time                     # Import time module
+import board                    # Import board module for pin definitions
+import adafruit_dht             # Import Adafruit DHT sensor library
+from trigger import classname   #call class of trigeer.py
+GPIO.setmode(GPIO.BCM)          # Set GPIO pin numbering mode
+quivalent to Arduino's setup()
+LED_pin = 2                     # LED pin is GPIO 2 on the Raspberry Pi
+GPIO.setup(LED_pin, GPIO.OUT)   # Set LED pin as output
+
+# Initialize the DHT22 sensor
+dhtDevice = adafruit_dht.DHT22(board.D4) #GPIO4
+
+def safe_print(*args, **kwargs):
+    """Prints safely, ignoring non-UTF-8 characters."""
+    try:
+        print(*args, **kwargs)
+    except UnicodeEncodeError:
+        print("Encoding error occurred while printing.")
+trigger_instance=classname()
+try:
+    while True:
+        trigger_value=trigger_instacne.trigger_value()# Call trigger_value method of the Trigger class
+        if trigger_value==1:
+            GPIO.output(LED_pin, GPIO.HIGH) # Turn on the LED
+            time.sleep(1)                   # Wait for 1 second
+        else:
+            GPIO.output(LED_pin, GPIO.LOW)  # Turn off the LED
+            time.sleep(1)                   # Wait for 1 second
+
+        try:
+            temperature_c = dhtDevice.temperature
+            temperature_f = temperature_c * (9 / 5) + 32
+            humidity = dhtDevice.humidity
+            safe_print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+                temperature_f, temperature_c, humidity))
+        except RuntimeError as error:
+            # Handle sensor errors
+            safe_print(error.args[0])
+        time.sleep(2.0)
+
+finally:                                # This block is executed when try block exits
+    GPIO.cleanup()                      # Reset GPIO pins
+
+
+# Setup section: e
