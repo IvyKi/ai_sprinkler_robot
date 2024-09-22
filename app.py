3304c2f8307d8477cdd5db3892ea3aee5ac88334
IvyKi <kinahae0331@tukorea.ac.kr>
2024-09-22 23:34:10 +0900

    chore: setting.py

--- app.py
+++ app.py
@@ -4,55 +4,28 @@ except ImportError:
     from mock_gpio import GPIO  # Import mock GPIO module
 
 import time  # Import time module
-import board  # Import board module for pin definitions
-import adafruit_dht  # Import Adafruit DHT sensor library
 import requests  # Import requests module for HTTP requests
 import json  # Import json module for data formatting
 import datetime as dt
+import setting
 import atexit
 
-# Pins for each sensor
-SENSOR_PINS = [4, 17, 27]  # DHT22 on GPIO 4, DHT11 on GPIO 17, 27
-PUMP = 3  # Pins for the pump
-MOTOR = 10  # Pin for the SG90 motor
 
-# Supabase RESTful API URL
-API_URL = "https://yscyyvxduwdfjldjnwus.supabase.co"
-# Supabase project API key
-API_KEY = (
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
-    ".eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl"
-    "zY3l5dnhkdXdkZmpsZGpud3VzIiwicm9sZSI"
-    "6ImFub24iLCJpYXQiOjE3MjIwNjYxMTQsImV"
-    "4cCI6MjAzNzY0MjExNH0.22vV2RlrW9TU92Y"
-    "79SzuOQKX8v8IISBcaHePht-43Q4"
-)
-# Supabase table names
-TABLE_NAME = ["action_log", "sprinkler_get", "sprinkler_get2", "sprinkler_get3"]
+# Updated motor angle (1번 -> 0도, 2번 -> 90도, 3번 -> 180도)
 
-# Initialize each sensor
-dht_sensors = {
-    SENSOR_PINS[0]: adafruit_dht.DHT22(board.D4, use_pulseio=False),  # DHT22 sensor
-    SENSOR_PINS[1]: adafruit_dht.DHT11(board.D17, use_pulseio=False),  # DHT11 sensor
-    SENSOR_PINS[2]: adafruit_dht.DHT11(board.D27, use_pulseio=False),  # DHT11 sensor
-}
-
-# Updated motor angle dictionary (1번 -> 0도, 2번 -> 90도, 3번 -> 180도)
-dictionary = {1: 0, 2: 90, 3: 180}  # Mapping of sensor numbers to motor angles
 
 servo_min_duty = 3  # Set the minimum duty cycle to 3
 servo_max_duty = 12  # Set the maximum duty cycle to 12
+servo = GPIO.PWM(setting.MOTOR, 50)
 
 
 def initialize_gpio():
     GPIO.cleanup()  # Initialize all GPIO ports
     GPIO.setmode(GPIO.BCM)  # Set GPIO pin numbering mode
-    GPIO.setup(SENSOR_PINS, GPIO.IN)  # Set SENSOR pins as input
-    GPIO.setup(PUMP, GPIO.OUT)  # Set PUMP pin as output
-    GPIO.setup(MOTOR, GPIO.OUT)  # Set MOTOR pin as output
+    GPIO.setup(setting.SENSOR_PINS, GPIO.IN)  # Set SENSOR pins as input
+    GPIO.setup(setting.PUMP, GPIO.OUT)  # Set PUMP pin as output
+    GPIO.setup(setting.MOTOR, GPIO.OUT)  # Set MOTOR pin as output
 
-    global servo
-    servo = GPIO.PWM(MOTOR, 50)  # Set MOTOR pin to PWM mode with 50Hz frequency
     servo.start(0)  # Initialize PWM with a duty cycle of 0
 
 
@@ -80,11 +53,11 @@ def safe_print(*args, **kwargs):
 
 
 def send_to_supabase(sensor_num: int, temp: float, humi: float):
-    url = f"{API_URL}/rest/v1/{TABLE_NAME[sensor_num]}"
+    url = f"{setting.API_URL}/rest/v1/{setting.TABLE[sensor_num]}"
     headers = {
         "Content-Type": "application/json",
-        "apikey": API_KEY,
-        "Authorization": f"Bearer {API_KEY}",
+        "apikey": setting.API_KEY,
+        "Authorization": f"Bearer {setting.API_KEY}",
     }
     payload = {
         "day": str(dt.datetime.today().date()),  # Convert date to string
@@ -101,9 +74,9 @@ def send_to_supabase(sensor_num: int, temp: float, humi: float):
 
 def check_sensor_conditions():
     triggered_sensors = []
-    for i, pin in enumerate(SENSOR_PINS):
+    for i, pin in enumerate(setting.SENSOR_PINS):
         try:
-            dht_device = dht_sensors[pin]
+            dht_device = setting.DHT_SENSORS[pin]
             temperature_c = dht_device.temperature
             humidity = dht_device.humidity
             send_to_supabase(i + 1, temperature_c, humidity)
@@ -113,8 +86,6 @@ def check_sensor_conditions():
 
             if temperature_c >= 25 and humidity >= 20:
                 triggered_sensors.append(i + 1)
-            else:
-                pass
 
         except RuntimeError as error:
             # Handle sensor errors
@@ -126,7 +97,7 @@ def check_sensor_conditions():
 def motor_angle(sensor_list):
     # Sensor list is sorted to ensure 1, 2, 3 order
     for sensor_number in sorted(sensor_list):
-        target_angle = dictionary[sensor_number]  # Get the target angle corresponding to the sensor number
+        target_angle = setting.ANGLE[sensor_number]  # Get the target angle corresponding to the sensor number
         safe_print(f"Moving motor to {target_angle}° for sensor {sensor_number}")
         set_servo_angle(target_angle)  # Move motor to the target angle
         time.sleep(1)
@@ -142,10 +113,10 @@ try:
         result = check_sensor_conditions()
         if result:
             motor_angle(result)
-            GPIO.output(PUMP, GPIO.HIGH)  # Turn pump on if condition is satisfied
+            GPIO.output(setting.PUMP, GPIO.HIGH)  # Turn pump on if condition is satisfied
             safe_print(f"Sensor {result} satisfied the condition. Pump is ON.")
             time.sleep(3)  # Keep the pump on for 3 seconds
-            GPIO.output(PUMP, GPIO.LOW)  # Turn pump off
+            GPIO.output(setting.PUMP, GPIO.LOW)  # Turn pump off
 
         else:
             safe_print("No sensor satisfied the condition. Pump remains OFF.")
--- setting.py
+++ setting.py
@@ -1,5 +1,7 @@
 from supabase import create_client, Client
 import datetime as dt
+import board  # Import board module for pin definitions
+import adafruit_dht  # Import Adafruit DHT sensor library
 
 
 API_URL = "https://yscyyvxduwdfjldjnwus.supabase.co"
@@ -11,7 +13,18 @@ API_KEY = (
     "4cCI6MjAzNzY0MjExNH0.22vV2RlrW9TU92Y"
     "79SzuOQKX8v8IISBcaHePht-43Q4")
 TABLE = ["action_log", "sprinkler_get", "sprinkler_get2", "sprinkler_get3", "sprinkler_get4"]
-PORT_NUM = [3, 4, 17, 27, 22]   # pump: 3, sensor: 4, 17, 27, 22
 TODAY = dt.datetime.today()
 SUPABASE: Client = create_client(API_URL, API_KEY)
 FILE_PATH = ['data001.xlsx', 'data002.xlsx']
+
+
+SENSOR_PINS = [4, 17, 27]  # DHT22 on GPIO 4, DHT11 on GPIO 17, 27
+PUMP = 3  # Pins for the pump
+MOTOR = 10  # Pin for the SG90 motor
+PORT_NUM = [3, 4, 17, 27, 22]   # pump: 3, sensor: 4, 17, 27, 22
+DHT_SENSORS = {
+    SENSOR_PINS[0]: adafruit_dht.DHT22(board.D4, use_pulseio=False),  # DHT22 sensor
+    SENSOR_PINS[1]: adafruit_dht.DHT11(board.D17, use_pulseio=False),  # DHT11 sensor
+    SENSOR_PINS[2]: adafruit_dht.DHT11(board.D27, use_pulseio=False),  # DHT11 sensor
+}
+ANGLE = {1: 0, 2: 90, 3: 180}  # Mapping of sensor numbers to motor angles
